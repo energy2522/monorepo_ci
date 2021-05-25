cd743940ff64d20dcf6bca6bf3547d2ea180ff13
energy2522 <volodymyr.maiboroda@gmail.com>
2021-05-25 14:35:02 +0300

    test

--- parse.py
+++ parse.py
@@ -4,5 +4,6 @@ if len(sys.argv) < 2:
     print("Should be at least input argument")
     exit(12)
 
-input_val = sys.argv[1]
-print([x.strip() for x in input_val.split(',')])
+print(sys.argv[1:len(sys.argv)])
+# input_val = sys.argv[1]
+# print([x.strip() for x in input_val.split(',')])
