a4082af77f0aa21c0d2ad8545fcbac24d85ca901
energy2522 <volodymyr.maiboroda@gmail.com>
2021-05-25 13:54:21 +0300

    test

--- parse_input.sh
+++ parse_input.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-a=($(echo "$1" | tr ', ' '\n'))
+a=($(echo "$1" | tr ',' '\n'))
 
 result="["
 i=0
 while [ $i -lt "${#a[@]}" ]; do
-  if [ $result != "[" ]; then
+  if [ "$result" != "[" ]; then
     result+=", "
   fi
 
   result+="\""${a[$i]}"\""
-  i+=1
+  ((i+=1))
 done
 
 result+="]"
