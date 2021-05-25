7e765ca6f6a4a90fbbf9490da5b87a82b61a0220
energy2522 <volodymyr.maiboroda@gmail.com>
2021-05-25 15:04:55 +0300

    test

--- parse.py
+++ parse.py
@@ -1,9 +1,10 @@
 import sys
+import json
 
 if len(sys.argv) < 2:
     print("Should be at least input argument")
     exit(12)
 
-print(sys.argv[1:len(sys.argv)])
+print(json.dumps(sys.argv[1:len(sys.argv)]))
 # input_val = sys.argv[1]
 # print([x.strip() for x in input_val.split(',')])
